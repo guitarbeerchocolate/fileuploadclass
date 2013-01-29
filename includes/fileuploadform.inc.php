0bb019da43bea5df0f7ecfa7fb440745253a55e1
mick <mick@mick-MT6833B.(none)>
2013-01-29 14:47:19 +0000

    Replaced posthandler with httphandler class.

--- includes/fileuploadform.inc.php
+++ includes/fileuploadform.inc.php
@@ -1,4 +1,4 @@
-<form enctype="multipart/form-data" action="posthandler.class.php" method="POST">
+<form enctype="multipart/form-data" action="httphandler.class.php" method="POST">
 <input name="method" type="hidden" value="singleuploadfile" />
 <input name="webpath" type="hidden" value="<?php echo $_SERVER['REQUEST_URI']; ?>" />
 <input name="uploaded" type="file" /><br />
